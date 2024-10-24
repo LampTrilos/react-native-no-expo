0b2ab99d624e4040246ea42483d9a3130786747d
Lampros <lamprostriantafyllopoulos@yahoo.com>
2024-10-24 19:02:22 +0300

    Sidebar always opens whenever you come back from a native screen, not sure i want that tbh
    Next: Check with Themos what the right object is
    Next: Fingerprints
    Next: Do the same for fingerprints

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,21 +1,22 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef, useCallback} from "react";
 import {
     Button, IconButton, Tooltip
 } from 'react-native-paper';
 import {Navigation} from '../utils/types.tsx';
 import {createDrawerNavigator} from "@react-navigation/drawer";
-import {DrawerActions} from '@react-navigation/native';
+import {DrawerActions, useFocusEffect} from '@react-navigation/native';
 import FaceCapture from "./FaceCapture.tsx";
 import Activation from "./Activation.tsx";
 import customTheme from '../assets/Theme'
 import CustomDrawerContent from "../components/TheDrawerContent.tsx";
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, AppState } from "react-native";
 import PassportNavigator from "../screens/DataGathering/PassportCheck/PassportNavigator.tsx"
 import {useDispatch, useSelector} from "react-redux";
 import {ControlStatusTypes} from "../utils/model.ts";
 import {setUser} from "../store/userData";
 import ControlStatus, {setStatus} from "../store/controlStatus";
 import BottomTabs from "./DataGathering/BottomTabs.tsx";
+
 
 type Props = {
     navigation: Navigation;
@@ -27,15 +28,24 @@ export default function Dashboard({navigation}: Props) {
     // On load of the page, we open the Drawer programmatically
     const drawerRef = useRef(null);
     useEffect(() => {
-        navigation.dispatch(DrawerActions.openDrawer());
-
-        //To force a trigger when we return to this screen from another screen
-        const unsubscribe = navigation.addListener('focus', () => {
+        const openDrawer = () => {
             navigation.dispatch(DrawerActions.openDrawer());
+        };
+        // On load of the page, we open the Drawer programmatically
+        openDrawer();
+
+        // Listener for app state changes
+        const appStateListener = AppState.addEventListener('change', (nextAppState) => {
+            if (nextAppState === 'active') {
+                // When the app comes to the foreground, trigger the function
+                openDrawer();
+            }
         });
+
         // Cleanup the listener when the component unmounts
-        return unsubscribe;
-        //Navigation is in the dependencies so as to force a trigger when we return to this screen from another screen
+        return () => {
+            appStateListener.remove();
+        };
     }, [navigation]);
 
     //Store section about the logged in user
@@ -49,6 +59,7 @@ export default function Dashboard({navigation}: Props) {
     useEffect(() => {
         dispatch(setStatus(ControlStatusTypes.None));
     }, []);
+
     //-------------------End of status store section------------------------//
     //The actual content of the drawer (links) is declared in TheDrawerContent.tsx--//
     return (
